98372ae5adb3c9ad3a2810bcfc721caca5c142d2
Julio Cesar <jccintr@gmail.com>
2023-08-28 21:24:00 -0300

    alterado tela login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
 import React, {useState,useContext,useEffect} from 'react';
-import {Flex,Box,FormControl,FormLabel,Input,Checkbox,Stack,Link,Button,Heading,Text,useColorModeValue,useToast } from '@chakra-ui/react';
+import {Flex,Box,FormControl,FormLabel,Input,Checkbox,Stack,Link,Button,Heading,Text,useColorModeValue,useToast,Image } from '@chakra-ui/react';
 import DataContext from '../../context/DataContext';
 import Api from '../../api/Api.js';
-
+import logo from '../../assets/logo-delivroo.png';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
@@ -53,16 +53,12 @@ useEffect(()=>{
 
 
     return (
-        <Flex
-          minH={'100vh'}
-          align={'center'}
-          justify={'center'}
-          bg={['white','gray.100']}
-          >
+        <Flex minH={'100vh'} align={'center'} justify={'center'} bg={['white','gray.100']}>
           <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
             <Stack align={'center'}>
-              <Heading fontSize={'4xl'}>BrazPed Admin</Heading>
-              <Text fontSize={'lg'} color={'gray.600'}>Informe as suas credenciais por favor.</Text>
+              <Image w='150px' h='150px' rounded='full'   src={logo} alt="logo" />
+              
+              <Text fontSize={'lg'} color={'gray.600'}>Painel de Administração</Text>
             </Stack>
             <Box w={['300px','400px']} rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={['none','lg']} p={[0,8]}>
               <Stack spacing={4}>
@@ -75,23 +71,13 @@ useEffect(()=>{
                   <Input type="password" value={password} onChange={e => setPassword(e.target.value)} />
                 </FormControl>
                 <Stack spacing={10}>
-                  <Stack
-                    direction={{ base: 'column', sm: 'row' }}
-                    align={'start'}
-                    justify={'space-between'}>
+                  <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
                     <Checkbox isChecked={remember} onChange={(e) => setRemember(e.target.checked)}>Permanecer conectado</Checkbox>
-                    
                   </Stack>
                   <Stack align={'center'}>
                      <Link color={'blue.400'}>Esqueceu a senha ?</Link>
                   </Stack>
-                  <Button 
-                    isLoading={isLoading}
-                    onClick={onLogin}
-                    bg={'blue.400'}
-                    color={'white'}
-                    _hover={{bg: 'blue.500',}}
-                  >
+                  <Button isLoading={isLoading} onClick={onLogin} bg={'blue.400'} color={'white'} _hover={{bg: 'blue.500',}}>
                     ENTRAR
                   </Button>
                 </Stack>
